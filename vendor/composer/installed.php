b305b6dab3f3b5f8ac8a2f9533f0cbfd40cf7230
Mathieu Dumont <mdumont@sio.local>
2020-11-10 11:40:36 +0100

    composer add

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e11e65b8e72e64c90af291dc71f04657fcb3858f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e11e65b8e72e64c90af291dc71f04657fcb3858f',
+    ),
+  ),
+);
